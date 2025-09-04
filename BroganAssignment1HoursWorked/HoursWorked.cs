b3180ac1db8363f3f1747aa9ce338376fcb74827
tzb00003 <tzb00003@mix.wvu.edu>
2025-09-04 09:39:38 -0400

    Add project files.

---
+++ BroganAssignment1HoursWorked/HoursWorked.cs
@@ -0,0 +1,15 @@
+namespace BroganAssignment1HoursWorked
+{
+    public partial class HoursWorked : Form
+    {
+        public HoursWorked()
+        {
+            InitializeComponent();
+        }
+
+        private void HoursWorked_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
